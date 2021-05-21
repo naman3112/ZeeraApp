8dc6beff17428992306753ca2aac582560c8a0fa
Naman <namanarora311@gmail.com>
2021-05-21 23:37:33 +0530

    ticket generator added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let TC = document.querySelector(".ticket-container");
 let allFilters = document.querySelectorAll(".filter");
 let modalVisible = false;
+let deleteButton = document.querySelector(".delete");
 let selectedPriority="pink";
 for (let i = 0; i < allFilters.length; i++) {
   allFilters[i].addEventListener("click", filterHandler);
@@ -16,19 +17,20 @@ addButton.addEventListener("click", showModal);
 
 function showModal(e) {
   if (!modalVisible) {
-    let modal = `<div class="modal">
-    <div class="task-to-be-added" data-type="false" contenteditable="true">
-        <span class="placeholder">Enter your text</span>
-    </div>
-    <div class="priority-list">
-        <div class="pink-modal-filter modal-filter active"></div>
-        <div class="blue-modal-filter modal-filter"></div>
-        <div class="green-modal-filter modal-filter"></div>
-        <div class="yellow-modal-filter modal-filter"></div>
+   let modal=document.createElement("div");
+   modal.classList.add("modal");    
+   modal.innerHTML=` <div class="task-to-be-added" data-type="false" contenteditable="true">
+   <span class="placeholder">Enter your text</span>
+                    </div>
+<div class="priority-list">
+   <div class="pink-modal-filter modal-filter active"></div>
+   <div class="blue-modal-filter modal-filter"></div>
+   <div class="green-modal-filter modal-filter"></div>
+   <div class="yellow-modal-filter modal-filter"></div>
 
-    </div>
-   `;
-    TC.innerHTML = TC.innerHTML + modal;
+</div>`;
+TC.appendChild(modal);
+   selectedPriority = "pink"
     let taskTyper = document.querySelector(".task-to-be-added");
     taskTyper.addEventListener("click", function (e) {
       if (e.currentTarget.getAttribute("data-type") == "false") {
@@ -54,17 +56,38 @@ function selectPriority(e) {
 }
 
 function addTicket(taskTyper,e) {
-  if (e.key == "Enter") {
-    let ticket = ` <div class="ticket">
-    <div class="ticket-color-${selectedPriority} ticket-color"></div>
-    <div class="ticket-id">kdkjd</div>
+  if (e.key == "Enter" && e.shiftKey==false && taskTyper.innerText.trim() != "") {
+    let ticket = document.createElement("div");
+    ticket.classList.add("ticket");
+    ticket.innerHTML=  `<div class="ticket-color-${selectedPriority} ticket-color"></div>
+    <div class="ticket-id"> ${uid()}</div>
     <div class="task">
        ${taskTyper.innerText}
-    </div>
-</div>`;
+    </div>`
+  document.querySelector(".modal").remove();
+  modalVisible = false;
+ 
+  ticket.addEventListener("click", function(e){
+    if(e.currentTarget.classList.contains("active")){
+      e.currentTarget.classList.remove("active");
+
+    }else{
+      e.currentTarget.classList.add("active");
+    }
+  })
+  TC.appendChild(ticket);
 console.log(ticket);
-document.querySelector(".modal").remove();
-TC.innerHTML=TC.innerHTML+ticket;
-modalVisible=false;
+
+
+}
+else if(e.key=="Enter" && e.shiftKey==false ){
+  e.preventDefault();
+  alert("you have not typed anything")
 }
 }
+deleteButton.addEventListener("click", function(e){
+  let selectedTickets=document.querySelectorAll(".ticket.active");
+  for(let i=0;i<selectedTickets.length; i++){
+    selectedTickets[i].remove();
+  }  
+})
